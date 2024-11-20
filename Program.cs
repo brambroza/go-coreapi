67ae4cc3d3cc6cab9c811332e54c03409d27a2f6
Amnart Kongpet <34531162+brambroza@users.noreply.github.com>
2024-11-20 22:57:03 +0700

    test upload file to docker

--- Program.cs
+++ Program.cs
@@ -67,6 +67,11 @@ if (app.Environment.IsDevelopment())
 {
   app.UseSwagger();
   app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "goalong api"));
+}else {
+app.UseSwagger();
+  app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "goalong api"));
+
+
 }
 
 app.UseCors("_MyAllowSpecificOrigins");
