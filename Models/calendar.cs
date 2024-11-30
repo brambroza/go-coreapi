80e57ad589f7228fcdfe0957ed13934b5c874d68
Amnart Kongpet <34531162+brambroza@users.noreply.github.com>
2024-11-30 11:42:48 +0700

    update quo repeatevery

--- Models/calendar.cs
+++ Models/calendar.cs
@@ -36,5 +36,7 @@ namespace coreapi.Models
     }
 
 
+ 
+
 
 }
---
+++ Models/repeatevery.cs
@@ -0,0 +1,18 @@
+
+namespace coreapi.Models
+{
+    public class RepeatEvery
+    {
+        public string UpdUser { get; set; }
+        public string RepeatEveryId { get; set; }
+        public string DocNo { get; set; }
+        public string DocType { get; set; }
+        public int RecurringEvery { get; set; }
+        public int IntervalType { get; set; }
+        public DateTime EveryDay { get; set; }
+        public string ExpiresType { get; set; }
+        public DateTime ExpiresDate { get; set; }
+        public int ExpiresCount { get; set; }
+        public string CmpId { get; set; }
+    }
+}
