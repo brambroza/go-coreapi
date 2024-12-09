97987f0d1004f257552988e27122e44fe9e56f10
Amnart Kongpet <34531162+brambroza@users.noreply.github.com>
2024-12-09 14:38:18 +0700

    update test image

--- Program.cs
+++ Program.cs
@@ -4,6 +4,7 @@ using Autofac.Extensions.DependencyInjection;
 using coreapi.Hubs;
 using goalongapi.Data;
 using goalongapi.Installers;
+
 // using goalongapi.Interfaces;
 // using goalongapi.Services;
 // using Microsoft.EntityFrameworkCore;
@@ -16,25 +17,29 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 builder.Services.AddSignalR();
-builder.Services.AddCors(p => p.AddPolicy("_MyAllowSpecificOrigins", builder =>
-{
-  /*  builder.WithOrigins("*").AllowAnyMethod().AllowAnyHeader();  */
-  builder.WithOrigins("http://nisolution.fortiddns.com:8284", "http://localhost:8080")
-         .AllowAnyMethod()
-         .AllowAnyHeader()
-         .AllowCredentials();
-
-}));
+builder.Services.AddCors(p =>
+    p.AddPolicy(
+        "_MyAllowSpecificOrigins",
+        builder =>
+        {
+            /*  builder.WithOrigins("*").AllowAnyMethod().AllowAnyHeader();  */
+            builder
+                .WithOrigins("http://nisolution.fortiddns.com:8284", "http://localhost:8080")
+                .AllowAnyMethod()
+                .AllowAnyHeader()
+                .AllowCredentials();
+        }
+    )
+);
 
 // Add services to the container.
 builder.Services.InstallServiceInAssembly(builder.Configuration);
- 
 
 /// google auth
-/// 
+///
 
 /*   var services = builder.Services;
-var configuration = builder.Configuration; 
+var configuration = builder.Configuration;
 services.AddAuthentication().AddGoogle(googleOptions =>
     {
         googleOptions.ClientId = "1096508373254-n72sf3a8ems41i43psu14dcsl9lifioa.apps.googleusercontent.com" ; // configuration["Authentication:Google:ClientId"];
@@ -42,12 +47,12 @@ services.AddAuthentication().AddGoogle(googleOptions =>
     }); */
 
 
-///end google auth 
+///end google auth
 
 
 
 
-// Call UseServiceProviderFactory on the Host sub property 
+// Call UseServiceProviderFactory on the Host sub property
 builder.Host.UseServiceProviderFactory(new AutofacServiceProviderFactory());
 
 // Option 1# to Add Service
@@ -56,18 +61,20 @@ builder.Host.UseServiceProviderFactory(new AutofacServiceProviderFactory());
 // Option 2# to Auto Add Services
 builder.Host.ConfigureContainer<ContainerBuilder>(builder =>
 {
-  builder.RegisterAssemblyTypes(Assembly.GetEntryAssembly())
-  .Where(t => t.Name.EndsWith("Service"))
-  .AsImplementedInterfaces();
+    builder
+        .RegisterAssemblyTypes(Assembly.GetEntryAssembly())
+        .Where(t => t.Name.EndsWith("Service"))
+        .AsImplementedInterfaces();
 });
 
-
 var app = builder.Build();
- if (app.Environment.IsDevelopment())
- {
-  app.UseSwagger();
-  app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "goalong api"));
- } 
+
+//if (app.Environment.IsDevelopment())
+//{
+app.UseSwagger();
+app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "goalong api"));
+
+// }
 
 app.UseCors("_MyAllowSpecificOrigins");
 app.UseStaticFiles();
@@ -77,14 +84,14 @@ app.UseRouting();
 
 app.UseAuthentication();
 app.UseAuthorization();
+
 /* app.MapControllers(); */
 app.UseEndpoints(endpoints =>
-    {
-      endpoints.MapControllers();
+{
+    endpoints.MapControllers();
 
-      // SignalR  
-      endpoints.MapHub<NotificationHub>("/notificationhub");
-    });
-
+    // SignalR
+    endpoints.MapHub<NotificationHub>("/notificationhub");
+});
 
 app.Run();
