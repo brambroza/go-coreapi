5e1bc0c1c8eb220b30d00606c0e58eb612f58f57
Amnart Kongpet <34531162+brambroza@users.noreply.github.com>
2024-12-30 00:26:51 +0700

    update rabbitmq config9

--- Services/RabbitMQService.cs
+++ Services/RabbitMQService.cs
@@ -1,4 +1,4 @@
-using System.Text;
+/* using System.Text;
 using coreapi.Controllers;
 using Newtonsoft.Json;
 using RabbitMQ.Client;
@@ -38,3 +38,4 @@ public class RabbitMQService
         );
     }
 }
+ */
