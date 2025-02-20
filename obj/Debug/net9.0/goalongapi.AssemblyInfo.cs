c5131df8bd63448336ca5a82baa02f1c722b5bbb
Amnart Kongpet <34531162+brambroza@users.noreply.github.com>
2025-02-20 12:27:09 +0700

    update api send mail6

--- obj/Debug/net9.0/goalongapi.AssemblyInfo.cs
+++ obj/Debug/net9.0/goalongapi.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("goalongapi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5b9b90f2a9eec21e3053108d87663c8fb2f323ad")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3ca45af89a51b8b864cb27ab3524b59b577e6584")]
 [assembly: System.Reflection.AssemblyProductAttribute("goalongapi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("goalongapi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
