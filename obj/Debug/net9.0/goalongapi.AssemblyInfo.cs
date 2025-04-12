db45632fe87629823b95a46004b67824a7b1103a
Amnart Kongpet <34531162+brambroza@users.noreply.github.com>
2025-04-12 21:01:45 +0700

    update reply task realtime

--- obj/Debug/net9.0/goalongapi.AssemblyInfo.cs
+++ obj/Debug/net9.0/goalongapi.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("goalongapi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+03638b5f2e3b17cf740c35d596a8c88fc0af879b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c462b8eb168594dd3db9163188b08d908f932674")]
 [assembly: System.Reflection.AssemblyProductAttribute("goalongapi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("goalongapi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
