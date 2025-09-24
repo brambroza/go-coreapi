7c75a981470bae173987617a4fb15663a82bd797
Amnart Kongpet <34531162+brambroza@users.noreply.github.com>
2025-09-24 12:28:45 +0700

    update cros

--- Program.cs
+++ Program.cs
@@ -26,7 +26,7 @@ builder.Services.AddCors(p =>
                      "http://nisolution.fortiddns.com:8284",
                      "http://localhost:8080",  "http://192.168.1.179:8080",
                      "http://192.168.55.219:8285", "http://10.0.2.2:8000",
-                     "http://127.0.0.1:51052" 
+                     "http://127.0.0.1:51052" , "https://liff.line.me"
                  )
                  .AllowAnyMethod()
                  .AllowAnyHeader()
--- obj/Debug/net9.0/goalongapi.AssemblyInfo.cs
+++ obj/Debug/net9.0/goalongapi.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("goalongapi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9b726a7f28498c7038a7e040a8d00ca103accf8f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dab00e1dadbfe0ec27ccd0be6ef6af4b2f6e351d")]
 [assembly: System.Reflection.AssemblyProductAttribute("goalongapi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("goalongapi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
